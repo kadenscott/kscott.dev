22813b25ea253fbb08a5e53769feae52147e4bb1
kadenscott <kscottdev@gmail.com>
2021-05-01 21:12:58 -0400

    style link container

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,22 @@
-<div class="image-container">
+<h1 class="page-title">kscott.dev</h1>
+
+<div class="icon-links">
+    <IconLink icon="{faTools}" title="CI" link="https://ci.kscott.dev"/>
+    <IconLink icon="{faList}" title="Reposilite" link="https://repo.kscott.dev"/>
     <IconLink icon="{faTools}" title="CI" link="https://ci.kscott.dev"/>
 </div>
 
 <script>
     import {faTools} from '@fortawesome/free-solid-svg-icons/faTools'
+    import {faList} from '@fortawesome/free-solid-svg-icons/faList'
     import IconLink from "../lib/IconLink.svelte";
 </script>
+
+<style>
+    .icon-links {
+        display: flex;
+        flex-direction: row;
+        max-width: 100vw;
+        justify-content: space-evenly;
+    }
+</style>
