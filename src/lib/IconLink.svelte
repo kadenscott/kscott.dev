ea0c207eb48cf983dd289e7a2d90a94875436fd4
kadenscott <kscottdev@gmail.com>
2021-05-01 20:39:14 -0400

    add project

---
+++ src/lib/IconLink.svelte
@@ -0,0 +1,28 @@
+<a href="{link}">
+    <div class="icon-link">
+        <div class="icon-link--icon">
+            <Icon data="{icon}"/>
+        </div>
+        <div class="icon-link--title">
+            <h3>{{title}}</h3>
+        </div>
+    </div>
+</a>
+
+<script>
+    import Icon from 'svelte-awesome'
+
+    export let icon;
+    export let title;
+    export let link;
+</script>
+
+<style>
+    .icon-link {
+        display: flex;
+    }
+
+    .icon-link--icon {
+
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<div class="image-container">
+    <IconLink icon="{faTools}" title="CI" link="https://ci.kscott.dev"/>
+</div>
+
+<script>
+    import {faTools} from '@fortawesome/free-solid-svg-icons/faTools'
+    import IconLink from "../lib/IconLink.svelte";
+</script>
